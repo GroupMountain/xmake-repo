a2aaa29408faa0ce1cf4f5574bb661b7321a48c1
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2024-05-27 22:55:02 +0800

    package(gmlib): add v0.12.7

--- packages/g/gmlib/xmake.lua
+++ packages/g/gmlib/xmake.lua
@@ -1,5 +1,6 @@
 package("gmlib")
     add_urls("https://github.com/GroupMountain/GMLIB/releases/download/v$(version)/SDK.zip")
+    add_versions("0.12.7", "100128c92da14ab8ba86ed2b260a8c04ef12c7ac25df62d8ef702b4fcf01b80b")
 
     on_install(function (package)
         os.cp("include", package:installdir())
