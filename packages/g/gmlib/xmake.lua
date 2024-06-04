78069e1c15631c21eb3f664decb54bb514b370a3
Kobe Bryant <116721335+KobeBryant114514@users.noreply.github.com>
2024-06-04 13:31:31 +0800

    repo(gmlib): add version v0.12.8

--- packages/g/gmlib/xmake.lua
+++ packages/g/gmlib/xmake.lua
@@ -1,6 +1,7 @@
 package("gmlib")
     add_urls("https://github.com/GroupMountain/GMLIB/releases/download/v$(version)/SDK.zip")
     add_versions("0.12.7", "100128c92da14ab8ba86ed2b260a8c04ef12c7ac25df62d8ef702b4fcf01b80b")
+    add_versions("0.12.8", "284e8e17523f7c07878bdb9af2cf7170a8306f826beea28f9b7cf87da7296e3d")
 
     on_install(function (package)
         os.cp("include", package:installdir())
