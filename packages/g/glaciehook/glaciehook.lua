cb9acc923478fbccaa42f0716e0ba95b605bc614
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2025-03-06 10:57:40 +0800

    package(glaciehook): add v0.0.1

---
+++ packages/g/glaciehook/glaciehook.lua
@@ -0,0 +1,11 @@
+package("glaciehook")
+    add_urls("https://github.com/GlacieTeam/GlacieHook/archive/refs/tags/v$(version).tar.gz", "https://github.com/GlacieTeam/GlacieHook.git")
+    add_versions("0.0.1", "ee47f6d86c424b475905619a45788e69c4ab3951317e81362bccbcafecf519f0")
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
