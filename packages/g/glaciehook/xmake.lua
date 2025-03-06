8d92a307fc15e25311aaf2749ef0bdbf1b51cf16
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2025-03-06 12:01:58 +0800

    package(glaciehook): fix v0.1.0

--- packages/g/glaciehook/xmake.lua
+++ packages/g/glaciehook/xmake.lua
@@ -3,6 +3,7 @@ package("glaciehook")
     add_versions("0.1.0", "cbb4cd7a3d26c988fcb5f6a404dfcc9b2fa1ed4e5f8f202160faec2d5605ad91")
 
     on_install(function (package)
+        os.cp("include", package:installdir())
         local configs = {}
         if package:config("static") then
             configs.kind = "static"
