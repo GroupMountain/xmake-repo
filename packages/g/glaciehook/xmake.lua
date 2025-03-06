c40386bd80c1bd5a7ffc26f458b7277f422f0dad
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2025-03-06 12:00:58 +0800

    package(glaciehook): add v0.1.0

--- packages/g/glaciehook/xmake.lua
+++ packages/g/glaciehook/xmake.lua
@@ -1,6 +1,6 @@
 package("glaciehook")
     add_urls("https://github.com/GlacieTeam/GlacieHook/archive/refs/tags/v$(version).tar.gz", "https://github.com/GlacieTeam/GlacieHook.git")
-    add_versions("0.0.1", "bf870291a7916755187304de2115c27892dca78f5e12fc0530d54d6fb8e133c9")
+    add_versions("0.1.0", "cbb4cd7a3d26c988fcb5f6a404dfcc9b2fa1ed4e5f8f202160faec2d5605ad91")
 
     on_install(function (package)
         local configs = {}
