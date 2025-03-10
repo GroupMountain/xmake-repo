8d9c7c5ee7381d1de904ef0b0e4a09fe1d87e5d6
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2025-03-10 22:02:48 +0800

    package(endstone): add v0.6.2

--- packages/e/endstone/xmake.lua
+++ packages/e/endstone/xmake.lua
@@ -3,6 +3,7 @@ package("endstone")
     add_urls("https://github.com/EndstoneMC/endstone/archive/refs/tags/v$(version).tar.gz", "https://github.com/EndstoneMC/endstone.git")
     add_versions("0.6.0", "d9d7f865629165230b6dfcb34b359186524f6784a7655f39a7140ca998455a38")
     add_versions("0.6.1", "d77cf3c6cf55c7df5c6e74e87ba6e371c3716778f53f1fa97d3eabe29555f91d")
+    add_versions("0.6.2", "5166c6e4bb9992c05eb5c7ea5c5b5c876011652bf48055287a75d6c83d0ab952")
 
     on_install("windows", "linux", function (package)
         os.cp("include", package:installdir())
