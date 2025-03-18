e0b2d62a935403c4c5e2b676f57fcf9f9a7cc2cb
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2025-03-18 08:21:09 +0800

    package(zstr): fix v1.0.7

--- packages/z/zstr/xmake.lua
+++ packages/z/zstr/xmake.lua
@@ -4,6 +4,5 @@ package("zstr")
     add_versions("1.0.7", "8d2ddae68ff7bd0a6fce6150a8f52ad9ce1bed2c4056c8846f4dec4f2dc60819")
 
     on_install("windows", "linux", function (package)
-        os.cp("src", package:installdir())
-        os.rename(path.join(package:installdir(), "src"), path.join(package:installdir(), "include"))
+        os.cp("src/*.hpp", package:installdir("include"))
     end)
