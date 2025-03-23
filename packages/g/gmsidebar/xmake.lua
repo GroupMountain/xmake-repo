9ad1178a70f1cc0f388ec05ecb98e94145d832e9
Zunsat Janvy <121496151+EpsilonZunsat@users.noreply.github.com>
2025-03-23 19:41:09 +0800

    Update gmsidebar

--- packages/g/gmsidebar/xmake.lua
+++ packages/g/gmsidebar/xmake.lua
@@ -1,6 +1,6 @@
 package("gmsidebar")
     add_urls("https://github.com/GroupMountain/GMSidebar/releases/download/v$(version)/SDK.zip")
-    add_versions("0.12.4", "bf5e79d1034c3dd793845cfc0b423134c5a6bd13bc8f202a48b81ecf83596228")
+    add_versions("0.13.11", "fde67755b4fae5bbf92002c7b5f4d2be3a23b4e85a6c9b2d152391988d8a3879")
 
     on_install(function (package)
         os.cp("include", package:installdir())
