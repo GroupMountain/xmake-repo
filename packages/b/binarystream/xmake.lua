094c629238796821ccfffc364962e72504490147
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2025-04-02 17:00:22 +0800

    package(binarystream): add v1.0.3

--- packages/b/binarystream/xmake.lua
+++ packages/b/binarystream/xmake.lua
@@ -3,6 +3,7 @@ package("binarystream")
     add_versions("1.0.0", "cdc1838bf90e7e1dd84870c363015aeb8c233e79b8c0d36576d2a55fe2bc5634")
     add_versions("1.0.1", "2c4d1452619792a26719866f1a184cc7db15ca2b634de975477d9710ed54529a")
     add_versions("1.0.2", "703f3af11c2a99bf41b3fb0b69b212c67a9750ce2ee77d8dea329d3588edbdf4")
+    add_versions("1.0.3", "ef0da9e3ce7b64d70a5d13d1246e380513a7209c0cccbc4aae41aa1a112d6e01")
 
     on_install(function (package)
         os.cp("include", package:installdir())
