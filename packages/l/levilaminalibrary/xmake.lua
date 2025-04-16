02d6c96d4b2b6f96fe38bbfa89f41dfdbcc6f411
n15421 <119112174+n15421@users.noreply.github.com>
2025-04-16 13:56:16 +0800

    LeviLaminaLibrary: v1.1.0

--- packages/l/levilaminalibrary/xmake.lua
+++ packages/l/levilaminalibrary/xmake.lua
@@ -4,7 +4,7 @@ package("levilaminalibrary")
     add_versions("0.13.5-fix", "3a38bdbf7600eb34f349a87a10a6af5f406098306472e4deca32c059d048da9d")
     add_versions("1.0.0", "6bec59d1049d04b0033b0ade692c633e408e1e9291d2b6f9a2faaa181c85d4c6")
     add_versions("1.0.1", "ae83983ad0e5b7a099c11e76c852f7233f5a719498c4dfaf1a2fe049211ddaed")
-
+    add_versions("1.1.0", "b748043afe30c810d412cb1090a290bd71f285131549c0afa7a55a519f663190")
     on_load(function(package)
         import("net.http")
         local deps_url = "https://github.com/GroupMountain/levilaminalibrary/releases/download/v" .. package:version():rawstr() .. "/deps.list" -- 实测这里使用v$(version)无法生效
