0a1dcdc18febc6edf488428b9a7f007f9d73b63e
子沐呀 <163636894+zimuya4153@users.noreply.github.com>
2025-04-29 19:43:41 +0800

    Update xmake.lua

--- packages/g/glaciehook/xmake.lua
+++ packages/g/glaciehook/xmake.lua
@@ -6,6 +6,7 @@ package("glaciehook")
 
     add_versions("1.0.0", "dc5259e6214e8cb8c1fd1c0d3abfd5b96b19c709a26d0e7edf155b0910a27a5d")
     add_versions("1.0.1", "d0f22a3b152af5763bef171dbf3e3b1d1ae63d5deb609a0ca93a8d2e27881760")
+    add_versions("1.0.2", "ae9186a044ff42b074f14e69018a8e6932f24e5502c69535a288e4b7fb491178")
 
     on_install(function (package)
         os.cp("include", package:installdir())
