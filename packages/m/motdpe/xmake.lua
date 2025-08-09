9f13607aa42f6203c25eb29f3f3bf1729623bb2b
DivanadiumPentaoxide <186297309+DivanadiumPentaoxide@users.noreply.github.com>
2025-08-10 02:22:58 +0800

    package(motdpe): add v1.0.03

---
+++ packages/m/motdpe/xmake.lua
@@ -0,0 +1,12 @@
+package("motdpe")
+    set_homepage("https://github.com/GlacieTeam/MotdPE")
+    add_urls("https://github.com/GlacieTeam/MotdPE/archive/refs/tags/v$(version).tar.gz")
+    set_license("MPL-2.0")
+
+    add_versions("1.0.0", "b9af8fcbac2d5abad4a2fbaafa507727dd9e58f3a46a97bf5b8a8314893bb408")
+
+    on_install(function (package)
+        os.cp("include", package:installdir())
+        local configs = {}
+        import("package.tools.xmake").install(package, configs)
+    end)
