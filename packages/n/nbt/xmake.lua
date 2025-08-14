f21dac49618ae229ff02e9aa3df0148bde86a52b
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2025-08-14 15:34:01 +0800

    package(binarystream): add v2.1.1

--- packages/n/nbt/xmake.lua
+++ packages/n/nbt/xmake.lua
@@ -3,7 +3,7 @@ package("nbt")
     add_urls("https://github.com/GlacieTeam/NBT/archive/refs/tags/v$(version).tar.gz")
     set_license("MPL-2.0")
 
-    add_deps("binarystream 2.0.0")
+    add_deps("binarystream 2.1.1")
 
     add_versions("2.0.0", "6fe89a47af94aab5add07b73771ba55faa845ec3f8719c123db8b3ca0431155c")
     add_versions("2.0.1", "d76cee6cf223c9bbcdbd00966f6bc753714b46a31de83f7b16b2b13e67eb89ee")
