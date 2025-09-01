ee7d0512d583de13b01d03017a826af8e05d7e3d
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2025-09-02 05:40:35 +0800

    package(zstr): rewrite

--- packages/z/zstr/xmake.lua
+++ packages/z/zstr/xmake.lua
@@ -1,11 +1,11 @@
 package("zstr")
     set_kind("library", { headeronly = true })
-    add_urls("https://github.com/mateidavid/zstr/archive/refs/tags/v$(version).tar.gz")
+    add_urls("https://github.com/GlacieTeam/zstr/archive/refs/tags/v$(version).tar.gz")
 
     add_deps("zlib 1.3.1")
 
-    add_versions("1.0.7", "8d2ddae68ff7bd0a6fce6150a8f52ad9ce1bed2c4056c8846f4dec4f2dc60819")
+    add_versions("1.0.8", "ed8f211375333e13b78e814d110b712d40435cc9407786be81336e399c552937")
 
     on_install("windows", "linux", function (package)
-        os.cp("src/*.hpp", package:installdir("include"))
+        os.cp("src/*.hpp", package:installdir("include/zstr"))
     end)
