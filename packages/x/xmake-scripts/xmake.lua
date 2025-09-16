37d98062a5f55eb93df427a4e4f7fcfef65e0ba6
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2025-09-16 12:49:23 +0800

    fix: fix typo

--- packages/x/xmake-scripts/xmake.lua
+++ packages/x/xmake-scripts/xmake.lua
@@ -1,4 +1,4 @@
-package("xmake-scipts")
+package("xmake-scripts")
     add_urls("https://github.com/GlacieTeam/XmakeScripts.git")
     set_kind("binary")
     add_versions("1.0.0", "f78b001d49bb764297630f4b0e2c356189c3cecd")
