6b3aa8633f453282bcb58429eb94f7b2c467d151
zimuya4153 <limulu10@163.com>
2025-10-04 23:53:18 +0800

    feat: update sha256

--- packages/m/modapi/xmake.lua
+++ packages/m/modapi/xmake.lua
@@ -1,9 +1,9 @@
 package("modapi")
-    add_urls("https://github.com/GroupMountain/ModApi-Release/releases/download/v$(version)/ModApi-SDK.zip", {alias = "A"})
-    add_versions("A:0.1.0", "3c26f83a0e578d6b57a61160dce4922324de9a2274141ffdfd194e1efdda0681")
+    add_urls("https://github.com/GroupMountain/ModAPI-Release/releases/download/v$(version)/ModAPI-SDK.zip", {alias = "A"})
+    add_versions("A:0.1.0", "62c8fe459ab9712d1e1d882716f829a9a95118361769b95428f8cc0ff34b4221")
 
-    add_urls("https://github.com/GroupMountain/ModApi-Release/archive/refs/tags/v$(version).tar.gz", {alias = "B"})
-    add_versions("B:0.1.0", "32a954590aa74b56245ec0ad44b3046be11df756fc56b7d050eb24770a884ba9")
+    add_urls("https://github.com/GroupMountain/ModAPI-Release/archive/refs/tags/v$(version).tar.gz", {alias = "B"})
+    add_versions("B:0.1.0", "f5d7bae5ab8d34ce934ba57c3f60c9efb9a54381aae6d962c1e893ec76671976")
 
     on_install(function (package)
         if os.isdir("include") then
