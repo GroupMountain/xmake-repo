1b9bcbabc18544f69067393e07cfcb3120995489
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2025-10-10 02:34:52 +0800

    fix: fix raknet package define

--- packages/r/raknet/xmake.lua
+++ packages/r/raknet/xmake.lua
@@ -8,8 +8,22 @@ package("raknet")
     add_versions("1.0.1", "baf637bcceb83ba10ebaa0c560bf24a5a85c47c6ff5669e844c47eee413982d0")
     add_versions("1.1.0", "aa8572e5729a82807698d203d3fb69a436a59c04fbbebe1b49f5799ec62cef40")
 
+    add_configs("ipv6", {description = "Enable IPv6 support", default = true, type = "boolean"})
+
     on_install(function (package)
         os.cp("include", package:installdir())
         local configs = {}
+        if package:config("ipv6") then 
+            package:add("defines", "RAKNET_SUPPORT_IPV6")
+        end
         import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_load(function (package)
+        if package:is_plat("windows") then
+            package:add("syslinks", "ws2_32")
+        end
+        if package:config("ipv6") then 
+            package:add("defines", "RAKNET_SUPPORT_IPV6")
+        end
     end)
