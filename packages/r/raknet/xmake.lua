a6d42348abafba5fe36088b2bd0cdc9a035fe53e
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2025-10-10 02:14:30 +0800

    package(raknet): add v1.1.0

--- packages/r/raknet/xmake.lua
+++ packages/r/raknet/xmake.lua
@@ -1,12 +1,12 @@
 package("raknet")
     set_homepage("https://github.com/EndGateMC/RakNet")
     set_description("RakNet Library")
-    set_license("LGPLv3")
 
     add_urls("https://github.com/EndGateMC/RakNet/archive/refs/tags/v$(version).tar.gz")
 
     add_versions("1.0.0", "5b0496aa3253b1e1c61489f5292c63fbd92130e3b29be528d43cc3fabfa998ab")
     add_versions("1.0.1", "baf637bcceb83ba10ebaa0c560bf24a5a85c47c6ff5669e844c47eee413982d0")
+    add_versions("1.1.0", "aa8572e5729a82807698d203d3fb69a436a59c04fbbebe1b49f5799ec62cef40")
 
     on_install(function (package)
         os.cp("include", package:installdir())
