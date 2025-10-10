7993b9672682f9921735da6496203c863969b3b2
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2025-10-11 03:51:41 +0800

    package(nbt): add v2.4.1

--- packages/n/nbt/xmake.lua
+++ packages/n/nbt/xmake.lua
@@ -26,6 +26,7 @@ package("nbt")
     add_versions("2.3.5", "0959137d7a9d359ded108d1454691604ae4e9b547354942f0da03d30f5500481")
     add_versions("2.3.6", "09ebbe5fa25d38e4f249c85ef368385811a0df341e9ab571ab5b72cc47b1f39b")
     add_versions("2.4.0", "cc04a2923e75fa58079628d4bd219b894f278a7e6f1e1eee8e31cb83652db956")
+    add_versions("2.4.1", "b73720ebd17d691689c5a44e2cc193cf2720a3cb3548a6b383dd1fc80166dbf4")
 
     on_install(function (package)
         os.cp("include", package:installdir())
