7d4da2bba783cd6745a0eb979975368ce7fed079
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2025-10-12 05:02:25 +0800

    package(xmake-scripts): add v1.1.0

--- packages/x/xmake-scripts/xmake.lua
+++ packages/x/xmake-scripts/xmake.lua
@@ -1,7 +1,10 @@
 package("xmake-scripts")
     add_urls("https://github.com/GlacieTeam/XmakeScripts.git")
     set_kind("binary")
+
     add_versions("1.0.0", "f78b001d49bb764297630f4b0e2c356189c3cecd")
+    add_versions("1.1.0", "f8b5ff6f9f64ebe978c4c0ffef3a4d0bfe5e107e")
+
     on_install(function (package)
         os.cp("rules/*", package:installdir("rules"))
     end)
