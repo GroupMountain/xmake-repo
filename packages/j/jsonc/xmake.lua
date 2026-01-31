1ab165c10da4a543232880e838397914aa90cae8
DivanadiumPentaoxide <186297309+DivanadiumPentaoxide@users.noreply.github.com>
2026-01-31 17:25:00 +0800

    package(jsonc): add v1.1.0

--- packages/j/jsonc/xmake.lua
+++ packages/j/jsonc/xmake.lua
@@ -8,6 +8,7 @@ package("jsonc")
     )
 
     add_versions("1.0.0", "b382d277090a27c3b9e63f49368bd55b7e12a6d78abb9e65f9d15ee5d89a275d")
+    add_versions("1.1.0", "1464668b13c13b810a3691b1f5b44971d2089c8530d2ed10a3ac6f9a4acc2989")
 
     on_install(function (package)
         os.cp("include", package:installdir())
