caa7349b07d40e3a5e68d003c6c1fe5dc567496b
DivanadiumPentaoxide <186297309+DivanadiumPentaoxide@users.noreply.github.com>
2026-02-02 23:13:24 +0800

    package(boost_multiprecision): add v1.89.0

--- packages/b/boost_multiprecision/xmake.lua
+++ packages/b/boost_multiprecision/xmake.lua
@@ -1,10 +1,10 @@
 package("boost_multiprecision")
     set_description("Boost.Multiprecision Module")
     set_kind("library", { headeronly = true })
-    set_urls("https://github.com/boostorg/multiprecision/archive/refs/tags/$(version).tar.gz")
+    set_urls("https://github.com/boostorg/multiprecision.git")
     set_license("BSL-1.0")
 
-    add_versions("Boost_1_89_0", "d6000cc58594fead142eee4bf3a1aec39bf144dcb6accd8f4b8e6d780d258482")
+    add_versions("1.89.0", "d6000cc58594fead142eee4bf3a1aec39bf144dcb6accd8f4b8e6d780d258482")
 
     on_install(function (package)
         os.cp("include/*", package:installdir("include"))
