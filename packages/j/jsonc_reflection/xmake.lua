d638ed082a4d5a27b9223e2a8d3ece29b838a0f2
DivanadiumPentaoxide <186297309+DivanadiumPentaoxide@users.noreply.github.com>
2026-02-07 11:16:57 +0800

    package(jsonc_reflection): add v1.2.0

--- packages/j/jsonc_reflection/xmake.lua
+++ packages/j/jsonc_reflection/xmake.lua
@@ -7,7 +7,7 @@ package("jsonc_reflection")
         "https://github.com/DivanadiumPentaoxide/JSONC-Reflection/archive/refs/tags/v$(version).tar.gz"
     )
 
-    add_deps("jsonc v1.1.0")
+    add_deps("jsonc v1.2.0")
     add_deps("boost_pfr 2.2.0")
     add_deps("magic_enum 0.9.7")
 
@@ -15,6 +15,7 @@ package("jsonc_reflection")
     add_versions("1.0.1", "1dea921d410075014447a40f66f27c1c291d0d22a149cacdef043b9c77d463c3")
     add_versions("1.1.0", "587b85efc11fa10130f90fa9b01345e67305dcd4c0edf964a598bb692e021f89")
     add_versions("1.1.1", "6d7d233f1746ce4aa46e3d9fbfbe0cb6b54c9bb92f4aa6e74e78a2b2fd14abfb")
+    add_versions("1.2.0", "ff469ea4afd6515b79dcf05934a09ed233999592021e6d7821ae82f42b3a116c")
 
     on_install(function (package)
         os.cp("include", package:installdir())
