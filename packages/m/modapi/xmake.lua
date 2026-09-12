ddbef6078fbe828c0f3a649fb3ad3c4f64ac86b7
killcerr <kill_cerr@outlook.com>
2026-09-12 20:47:16 +0800

    feat(modapi): add 26.40.0

--- packages/m/modapi/xmake.lua
+++ packages/m/modapi/xmake.lua
@@ -18,6 +18,7 @@ package("modapi")
     add_versions("B:0.4.0", "74d6212399fe71fc8c6b230edd9774bd416817249fcd37fb3a7dc3d4b7b4ebeb")
     add_versions("B:26.10.0", "42227aca2c0bba1ddb85c0c3390ee61579dc15f68f0ee3c83d51f31b7b35141e")
     add_versions("B:26.20.0", "4c53cac74e045a8148eff2e96af618feab86ee66e3063dfbf6f8941ddabda82e")
+    add_versions("B:26.40.0", "1a4ff95a26808e624437d9395d8270da04bd18ba08926301ccbe595907e43455")
 
     on_install(function (package)
         if os.isdir("include") then
